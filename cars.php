69e675f2dc12ed13ed1848800ba6d7478833a3ec
bouchentouf <fatimazoh86@gmail.com>
2024-02-04 00:45:59 +0100

    mon premier commit

---
+++ cars.php
@@ -0,0 +1,17 @@
+
+//header
+
+<?php
+$cars=[
+
+]
+
+$pdo= new PDO('mysql: host=localhost; bdname=progrt_baseDeDonnée');
+
+?>
+<?php
+
+$pdo = new PDO('pgsql:host=localhost;port=5432;dbname=nomBDD;user=utilisateur;password=motDePasse';
+
+
+
---
+++ templets/footer.php
@@ -0,0 +1,35 @@
+<!--debut footer-->
+<footer class="connecterFooter">
+        <div class="connecterFooter1">
+            <h2>contacter nous</h2>
+            <p>tel:4587965488522</p>
+            <p>envoyer un message en clicent <a href="formulaire.html">ici</a></p>
+           
+        </div>
+        <div class="connecterFooter2">
+            <h2>horaire douverture</h2>
+            <div>
+               
+               lun.  08:30 - 19:30 <br>
+               
+               mar.  08:30 - 19:30 <br>
+                
+               mer.  08:30 - 19:30  <br>
+                
+               jeu.  08:30 - 19:30  <br>
+                
+               vend. 08:30 - 19:30  <br>
+                
+               sam.  08:30 - 19:30  <br>
+                
+                dim. fermé
+                
+            </div>
+
+        </div>
+    </footer>
+</body>
+</html>
+
+
+<!--debut footer-->
