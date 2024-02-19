80fafffdbb16e42988f18ab6f50d4a404f31b4fe
bouchentouf <fatimazoh86@gmail.com>
2024-02-19 23:41:14 +0100

    ajoue de fichier

--- templets/carsCard.php
+++ templets/carsCard.php
@@ -3,20 +3,18 @@
       <div class="card" >
         <img class="card-img-top" src=<?=_CARS_IMG_PATH.$car['picture']?>  alt="Card image cap">
         <div class="card-body">
-          <h5 class="card-title"><?=$car['title']?></h5>
-          <p class="card-text"><?=$car['description']?></p>
+          <h5 class="card-title"><?=$car['title'];?></h5>
+          <p class="card-text"><?=$car['description'];?></p>
           <div class="container">
             <div ><p> <?=$car['price']?> </p></div>
             <div ><p> <?=$car['year']?> </p></div>
             <div ><p> <?=$car['km']?> </p></div>
           </div>
-          <a href="car.php?id=<?=$key;?>" class="btn btn-primary" > detailles</a>
+          <a href="car.php?id=<?=$car['id']; ?>" class="btn btn-primary" > detailles</a>
         </div>
       </div>
 
 </div>
-
-
 
 
 
